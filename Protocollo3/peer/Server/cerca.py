eb196db8730012ef82ab5577b8164a86b3418d27
baxi89 <andrea.bassi.wk@gmail.com>
2016-04-18 15:12:59 +0200

    Solite modifiche iniziali

--- Protocollo3/peer/Server/cerca.py
+++ Protocollo3/peer/Server/cerca.py
@@ -21,7 +21,7 @@ class CercaVicini (threading.Thread):
 	def run(self):
 
 		now = int(round(time.time()))
-		while (int(round(time.time())) - now) < 30:
+		while (int(round(time.time())) - now) < 30 and self.canRun:
 			try:
 				peers = self.app.db.getAllPeers()
 				if len(peers) != 0:
@@ -29,31 +29,52 @@ class CercaVicini (threading.Thread):
 					packetID = "".join(random.choice(chars) for x in range(random.randint(16, 16)))
 					for i in range(len(peers)):
 						ip , port = peers[i]
-						s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+
+						if random.randint(0,1)==0:
+							s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+							ip = ip.split("|")[0]
+						else:
+							s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+							ip = ip.split("|")[1]
 						s.connect((ip, int(port)))
-						print("sending near to " + str((ip, int(port))))
+
 						ttl = "04"
 						port_message = ("0" * (5-len(str(self.port)))) + str(self.port)
 						message = "SUPE" + packetID + self.address + port_message + ttl
-						print("SENDING " + message)
+
+						print("[SENDING] [SUPE]: " + message)
 						s.send(message)
 						s.close()
-				time.sleep(10)
+
+				time.sleep(20)
+
 			except:
 				self.numErr += 1
-				print("CERCAVICINI ERRORE", "ERR")
+				print("[ERROR] error in near", "ERR")
 				print(sys.exc_info()[0], "ERR")
 				print(sys.exc_info()[1], "ERR")
 				print(sys.exc_info()[2], "ERR")
 
-		print("PASSATI 10 SECONDI. FINE RICERCA")
-		self.app.peer.isSearching = False
-		#mi setto il superPeer a cui sono collegato
-		if not self.app.peer.iamsuper:
-			print("ABOUT TO CHOOSE SUPER PEER")
-			l = int(len(self.app.peer.superList))
-			print self.app.peer.superList, l
-			if l > 0:
-				index = random.randint(0, l-1)
-				self.app.peer.login(self.app.peer.superList[index])
+		if( self.canRun ):
+			print("[LOG] Searching finished. About to connect to supernode.")	
+			self.app.peer.isSearching = False
+
+			# Mi collego
+			if not self.app.peer.iamsuper:
+				print("[LOG] About to choose a peer")
+				l = int(len(self.app.peer.superList))
+				print self.app.peer.superList, l
+				if l > 0:
+					index = random.randint(0, l-1)
+					self.app.peer.login(self.app.peer.superList[index])
 		return
+
+
+
+
+
+
+
+
+
+
